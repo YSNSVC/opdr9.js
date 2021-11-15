a28b907cda8582b1e1c3818b3d70abc8d7bb84c2
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-15 18:17:23 +0100

    Add files via upload

--- opdr9.js
+++ opdr9.js
@@ -1,13 +1,53 @@
-const checkAge = function (theAge) {
-    console.log(true);
-    if ((theAge) >= 18) {
-        return theAge;
-    }
-
-    console.log(false)
-    if ((theAge) < 18) {
-
+const isAdult = function (age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
     }
 };
-checkAge(2);
 
+isAdult(19);
+
+const greet = function (age) {
+    if (isAdult(age)) {
+        return "Hello there";
+    } else {
+        return "Hey kiddo";
+    }
+};
+console.log(greet(65));
+console.log(greet(10));
+
+
+const isAdultShort = age => age >= 18;
+
+const greetShort = age => (isAdult(age) ? "Hello there" : "Hey kiddo");
+
+console.log(greetShort(65));
+console.log(greetShort(10));
+
+const calculateVAT = function (basePrice, VATPercentage) {
+    return basePrice * (VATPercentage / 100);
+};
+
+const calculatePriceIncludingVAT = function (basePrice, VATPercentage) {
+    const VAT = calculateVAT(basePrice, VATPercentage);
+    return basePrice + VAT;
+};
+
+console.log(calculatePriceIncludingVAT(1000, 21));
+console.log(calculatePriceIncludingVAT(2, 9));
+
+onst calculateBasePrice = function (priceIncludingVAT, VATPercentage) {
+    const basePrice = priceIncludingVAT / ((100 + VATPercentage) / 100);
+    return basePrice;
+};
+
+const calculateBasePriceAndVAT = function (priceIncludingVAT, VATPercentage) {
+    const basePrice = calculateBasePrice(priceIncludingVAT, VATPercentage);
+    const VAT = priceIncludingVAT - basePrice;
+    return [basePrice, VAT];
+};
+
+console.log(calculateBasePriceAndVAT(1210, 21)); 10]
+console.log(calculateBasePriceAndVAT(2.18, 9));
