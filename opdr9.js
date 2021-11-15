985850932e6b5199c2e46135953f6e3afa60b1d6
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-15 18:13:32 +0100

    Add files via upload

---
+++ opdr9.js
@@ -0,0 +1,13 @@
+const checkAge = function (theAge) {
+    console.log(true);
+    if ((theAge) >= 18) {
+        return theAge;
+    }
+
+    console.log(false)
+    if ((theAge) < 18) {
+
+    }
+};
+checkAge(2);
+
